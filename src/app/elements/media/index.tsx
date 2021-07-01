ff7381174b5ab64819c5f0002325763d5c5e2312
Matt Lucock <2455682+matthewlucock@users.noreply.github.com>
2021-07-01 11:59:05 +1000

    Collapse media wrapper component

--- src/app/elements/media/index.tsx
+++ src/app/elements/media/index.tsx
@@ -1,17 +1,25 @@
 import * as preact from 'preact'
 import { useLayoutEffect } from 'preact/hooks'
 import useEvent from '@react-hook/event'
+import clsx from 'clsx'
 import { view } from '@risingstack/react-easy-state'
 
-import { useMediaStore, useMedia } from '@/store'
+import styles from './styles.scss'
+
+import { useMediaStore, useMedia, useControlsStore } from '@/store'
 import mainProcess from '__main_process__'
 
-import { MediaWrapper } from './wrapper'
+import { NativeMedia } from './native'
+import { Controls } from '@/elements/controls'
+import { Shortcut } from '@/elements/shortcut'
+import { AutoplayBlockOverlay } from '@/elements/autoplay-block-overlay'
 import { AudioContent } from './audio-content'
 
 export const Media: preact.FunctionComponent = view(() => {
   const mediaStore = useMediaStore()
   const media = useMedia()
+  const controlsStore = useControlsStore()
+  const { activity } = controlsStore
 
   useEvent(document, 'keypress', (key): void => {
     if (key.code === 'Space') mediaStore.playPauseShortcut()
@@ -24,9 +32,21 @@ export const Media: preact.FunctionComponent = view(() => {
     }, [])
   }
 
+  const className = clsx(
+    styles.wrapper,
+    media.isVideo && styles.video,
+    !activity.showing && styles.controlsHidden
+  )
+
   return (
-    <MediaWrapper>
+    <div className={className} onClick={(): void => mediaStore.playPauseShortcut()}>
+      <NativeMedia />
+
+      {media.loaded && <Controls />}
+      <Shortcut />
+      <AutoplayBlockOverlay />
+
       {media.isAudio && <AudioContent />}
-    </MediaWrapper>
+    </div>
   )
 })
