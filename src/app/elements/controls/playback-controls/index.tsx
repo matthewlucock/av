5430a24b13021e80091ec4adf1f1c26deb651f51
Matt Lucock <2455682+matthewlucock@users.noreply.github.com>
2021-07-08 11:41:14 +1000

    Improve dialog button styles

--- src/app/elements/controls/playback-controls/index.tsx
+++ src/app/elements/controls/playback-controls/index.tsx
@@ -4,6 +4,7 @@ import { view } from '@risingstack/react-easy-state'
 import { PLAY_ICON, PAUSE_ICON } from '@/icons'
 import { useMedia } from '@/store'
 
+import { TooltipTarget } from '@/components/tooltip-target'
 import { ControlButton } from '@/components/controls/control-button'
 import { SpeedThrough, SpeedThroughShiftGroup } from '@/components/controls/speed-through'
 import { Skip } from '@/components/controls/skip'
@@ -18,9 +19,11 @@ export const PlaybackControls: preact.FunctionComponent = view(() => {
       </SpeedThroughShiftGroup>
       <SpeedThrough direction='backwards' active={media.rewinding} />
 
-      <ControlButton primary onClick={(): void => media.playPause()}>
-        {media.playing ? PAUSE_ICON : PLAY_ICON}
-      </ControlButton>
+      <TooltipTarget tooltip='Play/pause'>
+        <ControlButton primary onClick={(): void => media.playPause()}>
+          {media.playing ? PAUSE_ICON : PLAY_ICON}
+        </ControlButton>
+      </TooltipTarget>
 
       <SpeedThrough direction='forwards' active={media.fastForwarding} />
       <SpeedThroughShiftGroup direction='forwards' active={media.fastForwarding}>
